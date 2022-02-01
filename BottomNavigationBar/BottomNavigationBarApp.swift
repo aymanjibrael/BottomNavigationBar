8509d87ad538989e9bf7061bd673f091824a1dfa
aymanjibrael <ayman.jibrael@icloud.com>
2022-02-01 15:03:34 +0300

    Initial Commit

---
+++ BottomNavigationBar/BottomNavigationBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  BottomNavigationBarApp.swift
+//  BottomNavigationBar
+//
+//  Created by Ayman Jibrael on 1.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct BottomNavigationBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BottomNavigationBar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BottomNavigationBar
+//
+//  Created by Ayman Jibrael on 1.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
